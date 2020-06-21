4f47146612c336f923f32edd94b6690d2ad4fd25
Stefan Milikic <stefanmilikic999@gmail.com>
2020-06-21 11:36:00 +0200

    Updates

---
+++ precache-manifest.edfbd4121c2ded897e3cc914f20b2465.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf8c036a538e0ecde2c572472702be9",
+    "url": "/EducationPlatform/index.html"
+  },
+  {
+    "revision": "b091379685ceb103f54f",
+    "url": "/EducationPlatform/static/css/2.73dfdab8.chunk.css"
+  },
+  {
+    "revision": "f933ab45174fa55a461a",
+    "url": "/EducationPlatform/static/css/main.67dc8efc.chunk.css"
+  },
+  {
+    "revision": "b091379685ceb103f54f",
+    "url": "/EducationPlatform/static/js/2.d6eca547.chunk.js"
+  },
+  {
+    "revision": "d101784e6414d4293d250fa98b88d65a",
+    "url": "/EducationPlatform/static/js/2.d6eca547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f933ab45174fa55a461a",
+    "url": "/EducationPlatform/static/js/main.d26dbb7c.chunk.js"
+  },
+  {
+    "revision": "f32ce0e50ebb12f0489c",
+    "url": "/EducationPlatform/static/js/runtime-main.5988cf17.js"
+  },
+  {
+    "revision": "8620fb56413357223bbf96fcf04ed907",
+    "url": "/EducationPlatform/static/media/avatar1.8620fb56.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/EducationPlatform/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/EducationPlatform/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/EducationPlatform/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/EducationPlatform/static/media/slick.f97e3bbf.svg"
+  }
+]);
