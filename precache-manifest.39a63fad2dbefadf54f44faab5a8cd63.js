4e1ef66d9766690e09a7d04fc084587675805abb
Stefan Milikic <stefanmilikic999@gmail.com>
2020-06-21 13:03:33 +0200

    Updates

---
+++ precache-manifest.39a63fad2dbefadf54f44faab5a8cd63.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "664374e51d461e707de0a12cb81bdec4",
+    "url": "/EducationPlatform/index.html"
+  },
+  {
+    "revision": "796a9f6f13e0eb10a8b4",
+    "url": "/EducationPlatform/static/css/2.18899b43.chunk.css"
+  },
+  {
+    "revision": "17738138eaa779bf557c",
+    "url": "/EducationPlatform/static/css/main.10902bef.chunk.css"
+  },
+  {
+    "revision": "796a9f6f13e0eb10a8b4",
+    "url": "/EducationPlatform/static/js/2.f4a86809.chunk.js"
+  },
+  {
+    "revision": "d101784e6414d4293d250fa98b88d65a",
+    "url": "/EducationPlatform/static/js/2.f4a86809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17738138eaa779bf557c",
+    "url": "/EducationPlatform/static/js/main.8fc56660.chunk.js"
+  },
+  {
+    "revision": "f32ce0e50ebb12f0489c",
+    "url": "/EducationPlatform/static/js/runtime-main.5988cf17.js"
+  },
+  {
+    "revision": "8620fb56413357223bbf96fcf04ed907",
+    "url": "/EducationPlatform/static/media/avatar1.8620fb56.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/EducationPlatform/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/EducationPlatform/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/EducationPlatform/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/EducationPlatform/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ed63f35db647af02920f0308e47adf2.js"
+  "/EducationPlatform/precache-manifest.39a63fad2dbefadf54f44faab5a8cd63.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/EducationPlatform/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
