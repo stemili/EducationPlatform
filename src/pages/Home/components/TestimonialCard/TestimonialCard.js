0da7cbc0fb403053ea056d92810aa1691f944a59
rad1na <radinovicpetar98@gmail.com>
2020-06-24 01:44:14 +0200

    More functionality on testimonial add

--- src/pages/Home/components/TestimonialCard/TestimonialCard.js
+++ src/pages/Home/components/TestimonialCard/TestimonialCard.js
@@ -3,7 +3,7 @@ import Swiper from "swiper";
 import "swiper/css/swiper.min.css";
 import "./TestimonialCard.css";
 import axios from "axios";
-import { Modal, Button, Input } from "antd";
+import { Modal, Button, Input, Result } from "antd";
 import Auth from "../../../../auth/AuthService";
 
 export default class TestimonialCards extends React.Component {
@@ -15,6 +15,8 @@ export default class TestimonialCards extends React.Component {
     currentUser: Auth.getCurrentUser(),
     textArea: "",
     editTestimonial: false,
+    showSuccess: false,
+    showError: false,
   };
   componentDidMount() {
     axios
@@ -108,8 +110,8 @@ export default class TestimonialCards extends React.Component {
           username: `${this.state.currentUser.username}`,
           text: `${this.state.textArea}`,
         })
-        .then((res) => this.setState({ loading: false }))
-        .catch((err) => console.log(err.message));
+        .then((res) => this.setState({ loading: false, showSuccess: true }))
+        .catch((err) => this.setState({ showError: true, loading: false }));
     } else {
       axios
         .put(
@@ -117,71 +119,104 @@ export default class TestimonialCards extends React.Component {
           { text: `${this.state.textArea}` }
         )
         .then((res) => {
-          console.log(res);
-          this.setState({ loading: false });
+          this.setState({ loading: false, showSuccess: true });
+        })
+        .catch(() => {
+          this.setState({ showError: true, loading: false });
         });
     }
   };
 
   handleCancel = () => {
-    this.setState({ visible: false });
+    this.setState({ visible: false, showSuccess: false, showError: false });
   };
 
   onTextChange = (e) => {
     this.setState({ textArea: e.target.value });
   };
 
+  handleReturn = () => {
+    this.setState({ showSuccess: false, showError: false });
+  };
+
+  checkForAnswer = () => {
+    if (this.state.showSuccess === true) {
+      return <Result status="success" title="Successfully updated review!" />;
+    } else if (this.state.showError === true) {
+      return (
+        <Result
+          status="error"
+          title="Oops something went wrong. Please try again!"
+        />
+      );
+    } else {
+      const { TextArea } = Input;
+      return (
+        <React.Fragment>
+          <p>Maximum number of characters is: 180</p>
+          <TextArea
+            rows={6}
+            value={this.state.textArea}
+            onChange={(e) => this.onTextChange(e)}
+            maxLength="180"
+          />
+        </React.Fragment>
+      );
+    }
+  };
+
   render() {
     const { visible, loading } = this.state;
-    const { TextArea } = Input;
     return (
-      <div className="testimonial-section">
-        <h1>What do people say about us?</h1>
-        <div className="swiper-container">
-          <div className="swiper-wrapper">{this.renderTestimonials()}</div>
-          <div
-            className="swiper-button-next"
-            onClick={(e) => this.handleClick(e)}
-          ></div>
-          <div
-            className="swiper-button-prev"
-            onClick={(e) => this.handleClick(e)}
-          ></div>
-        </div>
-        <div className="testimonial-text-box fadeIn">
-          <h2>
-            {this.state.testimonials.length !== 0
-              ? this.state.testimonials[this.state.current].name +
-                " " +
-                this.state.testimonials[this.state.current].surname
-              : ""}
-          </h2>
-          <i className="fas fa-quote-left"></i>
-          <i className="fas fa-caret-down"></i>
-          <span>
-            {this.state.testimonials.length !== 0
-              ? this.state.testimonials[this.state.current].text
-              : ""}
-          </span>
-          <i className="fas fa-quote-right"></i>
+      <React.Fragment>
+        <div className="testimonial-section">
+          <h1>What do people say about us?</h1>
+          <div className="swiper-container">
+            <div className="swiper-wrapper">{this.renderTestimonials()}</div>
+            <div
+              className="swiper-button-next"
+              onClick={(e) => this.handleClick(e)}
+            ></div>
+            <div
+              className="swiper-button-prev"
+              onClick={(e) => this.handleClick(e)}
+            ></div>
+          </div>
+          <div className="testimonial-text-box fadeIn">
+            <h2>
+              {this.state.testimonials.length !== 0
+                ? this.state.testimonials[this.state.current].name +
+                  " " +
+                  this.state.testimonials[this.state.current].surname
+                : ""}
+            </h2>
+            <i className="fas fa-quote-left"></i>
+            <i className="fas fa-caret-down"></i>
+            <span>
+              {this.state.testimonials.length !== 0
+                ? this.state.testimonials[this.state.current].text
+                : ""}
+            </span>
+            <i className="fas fa-quote-right"></i>
+          </div>
         </div>
         <div className="add-testimonial-section">
-          <h2>
-            Want to get featured? Click below to add your opinion about our
-            website!
-          </h2>
+          <h1>Want to get featured?</h1>
+          <h3>Click below to add your opinion about our website!</h3>
           <button onClick={this.showModal}>
-            {this.state.editTestimonial === true ? "Edit" : "Add"} a review
+            {this.state.editTestimonial === true ? "Edit your" : "Add a"} review
           </button>
           <Modal
             visible={visible}
             title={`${
-              this.state.editTestimonial === true ? "Edit" : "Add"
-            } a review`}
+              this.state.editTestimonial === true ? "Edit your" : "Add a"
+            } review`}
             onOk={this.handleOk}
             onCancel={this.handleCancel}
+            height="400"
+            centered
             footer={[
-              <Button key="back" onClick={this.handleCancel}>
+              <Button key="back" onClick={this.handleReturn}>
                 Return
               </Button>,
               <Button
@@ -194,16 +229,10 @@ export default class TestimonialCards extends React.Component {
               </Button>,
             ]}
           >
-            <p>Maximum number of words: 180</p>
-            <TextArea
-              rows={4}
-              value={this.state.textArea}
-              onChange={(e) => this.onTextChange(e)}
-              maxLength="180"
-            />
+            {this.checkForAnswer()}
           </Modal>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
