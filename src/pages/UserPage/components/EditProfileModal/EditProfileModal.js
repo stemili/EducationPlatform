13e2a57026c6ce635ee58685663d28851c180d36
Stefan Milikic <stefanmilikic999@gmail.com>
2020-06-23 17:05:32 +0200

    edit user profile added, not completed

---
+++ src/pages/UserPage/components/EditProfileModal/EditProfileModal.js
@@ -0,0 +1,108 @@
+import React, { useEffect } from "react";
+import { Modal, Form, Input, DatePicker } from "antd";
+import AuthService from "../../../../auth/AuthService";
+import "./EditProfileModal.css";
+
+const EditProfileModal = ({
+  editProfileModal,
+  setEditProfileModal,
+  onCreate,
+  formRef,
+}) => {
+  //   useEffect(() => {
+  //     if (formRef.current) {
+  //       console.log(formRef.current);
+  //       formRef.current.setFieldsValue({
+  //         name: AuthService.getCurrentUser().name,
+  //         surname: AuthService.getCurrentUser().surname,
+  //         location: AuthService.getCurrentUser().location,
+  //         dateOfBirth: AuthService.getCurrentUser().date_of_birth,
+  //         phone: AuthService.getCurrentUser().phone,
+  //       });
+  //     }
+  //   }, []);
+  const currentUser = AuthService.getCurrentUser();
+  return (
+    <Modal
+      title="Edit Profile"
+      visible={editProfileModal}
+      onOk={onCreate}
+      onCancel={() => setEditProfileModal(false)}
+      className="edit-img-modal"
+      okText="Save Changes"
+    >
+      <Form
+        layout="vertical"
+        ref={formRef}
+        initialValues={{
+          name: currentUser.name,
+          surname: currentUser.surname,
+          location: currentUser.location,
+          phone: currentUser.phone,
+        }}
+      >
+        <Form.Item
+          name="name"
+          label="First Name"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Last Name"
+          name="surname"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Last Name!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item label="Phone number" name="phone">
+          <Input placeholder="Phone Number" />
+        </Form.Item>
+        <Form.Item label="Location" name="location">
+          <Input placeholder="Country/City" />
+        </Form.Item>
+        <Form.Item label="Date of Birth" name="dateOfBirth">
+          <DatePicker />
+        </Form.Item>
+        <p className="edit-profile-change-pass">Change Password</p>
+        <Form.Item name="oldPassword">
+          <Input.Password placeholder="Current Password" />
+        </Form.Item>
+        <Form.Item name="password" label="New Password">
+          <Input.Password placeholder="Password" />
+        </Form.Item>
+        <Form.Item
+          name="confirm"
+          // label="Confirm Password"
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            ({ getFieldValue }) => ({
+              validator(rule, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  "The two passwords that you entered do not match!"
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password placeholder="Confirm Password" />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default EditProfileModal;
--- src/pages/UserPage/index.js
+++ src/pages/UserPage/index.js
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { Link } from "react-router-dom";
-import { Modal, Input } from "antd";
+import { Modal, Input, Form, DatePicker, message } from "antd";
 
 import AuthService from "../../auth/AuthService";
 import "./index.css";
 import StudentPanel from "./components/StudentPanel/StudentPanel";
 import TeacherPanel from "./components/TeacherPanel/TeacherPanel";
+import EditProfileModal from "./components/EditProfileModal/EditProfileModal";
 import { EditOutlined } from "@ant-design/icons";
 import axios from "axios";
+import { useEffect } from "react";
 
 const UserPage = () => {
   const currentUser = AuthService.getCurrentUser();
+  const formRef = React.createRef();
+  const [imageEditModal, setImageEditModal] = useState(false);
+  const [editProfileModal, setEditProfileModal] = useState(false);
 
-  const [editModal, setEditModal] = useState(false);
   const [imgUrlUpdate, setImgUrlUpdate] = useState("");
+
   const renderUserPanel = () => {
     if (currentUser.role_id === "student") {
       return <StudentPanel />;
@@ -28,12 +33,12 @@ const UserPage = () => {
     }
   };
 
-  const showModal = () => {
-    setEditModal(true);
+  const showImageModal = () => {
+    setImageEditModal(true);
   };
 
-  const confirmModal = e => {
-    setEditModal(false);
+  const confirmImageModal = e => {
+    setImageEditModal(false);
     axios
       .put(
         `https://courses4me.herokuapp.com/users/${
@@ -61,8 +66,69 @@ const UserPage = () => {
       });
   };
 
+  const confirmEditProfileModal = e => {
+    const formValues = formRef.current.getFieldsValue();
+    const dobMain = formValues.dateOfBirth
+      ? formValues.dateOfBirth._d.toString().slice(4, 15)
+      : currentUser.date_of_birth;
+    const changeUserInfoData = {
+      name: formValues.name,
+      surname: formValues.surname,
+      location: formValues.location,
+      dateOfBirth: dobMain,
+      phone: formValues.phone,
+    };
+    if (formValues.password === formValues.confirm) {
+      if (formValues.oldPassword !== "" && formValues.password !== "") {
+        console.log("we are changing passwords");
+        const changePassData = {
+          oldPassword: formValues.oldPassword,
+          newPassword: formValues.password,
+        };
+
+        console.log(formValues.password);
+        const passwordChangeReq = axios
+          .put(
+            `https://courses4me.herokuapp.com/users/${currentUser.username}/password`,
+            changePassData,
+            {
+              headers: {
+                authorization: AuthService.getAuthHeader(),
+              },
+            }
+          )
+          .then(res => message.success("Password Successfully changed!"))
+          .catch(err => {
+            message.error(err.response.data.error);
+          });
+        const userInfoChangeReq = axios
+          .put(
+            `https://courses4me.herokuapp.com/users/${currentUser.username}`,
+            changeUserInfoData,
+            {
+              headers: {
+                authorization: AuthService.getAuthHeader(),
+              },
+            }
+          )
+          .then(res => message.success("User Profile Updated!"))
+          .catch(err => message.error(err.response.data.error));
+
+        axios.all([userInfoChangeReq, passwordChangeReq]).finally(res => {
+          setEditProfileModal(false);
+        });
+        // .catch(errors => {
+        //   let firstResponse =  errors[0];
+        //   let secondResponse = errors[1];
+        //   message.error()
+        // })
+      }
+    }
+  };
+
   const cancelModal = e => {
-    setEditModal(false);
+    setImageEditModal(false);
+    setEditProfileModal(false);
   };
 
   const handleImgUrlChange = e => {
@@ -82,19 +148,29 @@ const UserPage = () => {
               }
               alt="avatar"
             />
-            <div className="edit-image-btn" onClick={showModal}>
+            <div className="edit-image-btn" onClick={showImageModal}>
               <EditOutlined />
             </div>
           </div>
           <Modal
             title="Update Profile Image"
-            visible={editModal}
-            onOk={confirmModal}
+            visible={imageEditModal}
+            onOk={confirmImageModal}
             onCancel={cancelModal}
             className="edit-img-modal"
           >
             <Input value={imgUrlUpdate} onChange={handleImgUrlChange}></Input>
           </Modal>
+
+          <EditProfileModal
+            formRef={formRef}
+            editProfileModal={editProfileModal}
+            setEditProfileModal={setEditProfileModal}
+            onCreate={confirmEditProfileModal}
+          />
+          {/*proba 1*/}
+
+          {/*proba 1*/}
           <div className="profile-about-user">
             <h3>
               {currentUser.name + " " + currentUser.surname}
@@ -114,7 +190,10 @@ const UserPage = () => {
               <i className="fas fa-map-marker"></i> Podgorica (static)
             </p>
             <div className="profile-btn-section">
-              <button className="btn edit-profile-btn disabled">
+              <button
+                className="btn edit-profile-btn"
+                onClick={() => setEditProfileModal(true)}
+              >
                 Edit Profile
               </button>
               {currentUser.role_id === "administrator" ? (
